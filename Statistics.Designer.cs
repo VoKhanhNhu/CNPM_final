32673d6a6fb60b3edc737077c6be21cbc09522c8
VoKhanhNhu <128213905+VoKhanhNhu@users.noreply.github.com>
2023-05-09 20:29:01 +0700

    Add files via upload

---
+++ Statistics.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace VKN
+{
+    partial class Statistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goodsOutDGV = new System.Windows.Forms.DataGridView();
+            this.salesDataGridView = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.goodsOutDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // goodsOutDGV
+            // 
+            this.goodsOutDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.goodsOutDGV.Location = new System.Drawing.Point(25, 131);
+            this.goodsOutDGV.Name = "goodsOutDGV";
+            this.goodsOutDGV.RowHeadersWidth = 51;
+            this.goodsOutDGV.RowTemplate.Height = 24;
+            this.goodsOutDGV.Size = new System.Drawing.Size(532, 185);
+            this.goodsOutDGV.TabIndex = 0;
+            // 
+            // salesDataGridView
+            // 
+            this.salesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.salesDataGridView.Location = new System.Drawing.Point(25, 452);
+            this.salesDataGridView.Name = "salesDataGridView";
+            this.salesDataGridView.RowHeadersWidth = 51;
+            this.salesDataGridView.RowTemplate.Height = 24;
+            this.salesDataGridView.Size = new System.Drawing.Size(532, 185);
+            this.salesDataGridView.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox1.Location = new System.Drawing.Point(25, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(493, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(303, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Statistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 649);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.salesDataGridView);
+            this.Controls.Add(this.goodsOutDGV);
+            this.Name = "Statistics";
+            this.Text = "Statistics";
+            ((System.ComponentModel.ISupportInitialize)(this.goodsOutDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView goodsOutDGV;
+        private System.Windows.Forms.DataGridView salesDataGridView;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Statistics.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using DataTable = System.Data.DataTable;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+using ExcelDataReader;
+using ExcelDataReader.Exceptions;
+using System.Drawing.Printing;
+using Font = System.Drawing.Font;
+
+namespace VKN
+{
+    public partial class Statistics : Form
+    {
+        public Statistics()
+        {
+            InitializeComponent();
+        }
+
+        public int CheckGoodsOut(string ID, int month)
+        {
+            int re = 0;
+            var fileInfo = new FileInfo(@"D:\NT106\VKN\order.xlsx");
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                int rowCount = worksheet.Dimension.Rows;
+                int colCount = worksheet.Dimension.Columns;
+
+                // Duyệt qua từng dòng để kiểm tra thông tin đăng nhập
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    DateTime d = (DateTime) worksheet.Cells[row, 2].Value;
+                    if (ID == worksheet.Cells[row, 3].Value?.ToString() && month == d.Month && d.Year == 2023) 
+                        re += int.Parse(worksheet.Cells[row, 5].Value.ToString());
+                }
+            }
+            return re;
+        }
+
+        public void GenerateMonthlyReport(object month)
+        {
+            var fileInfo = new FileInfo(@"D:\NT106\VKN\goods.xlsx");
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                int rowCount = worksheet.Dimension.Rows;
+                int colCount = worksheet.Dimension.Columns;
+                
+                int m = int.Parse((string)month);
+
+                int bestSaler = 0;
+                long revenue = 0;
+                DataTable goodsOut = new DataTable();
+                goodsOut.Columns.Add("Goods ID");
+                goodsOut.Columns.Add("Goods Name");
+                goodsOut.Columns.Add("This month's sale");
+                // Duyệt qua từng dòng để kiểm tra thông tin đăng nhập
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    int x = CheckGoodsOut(worksheet.Cells[row, 1].Value?.ToString(), m);
+                    int check = Math.Max(bestSaler, x);
+                    revenue += x * long.Parse(worksheet.Cells[row, 4].Value?.ToString());
+                    goodsOut.Rows.Add(worksheet.Cells[row, 1].Value?.ToString(), worksheet.Cells[row, 2].Value?.ToString(), x.ToString());
+                    if (check != bestSaler && check != 0)
+                    {
+                        textBox1.Text = "This month's best saler: " + worksheet.Cells[row, 2].Value?.ToString() + "(Goods ID: " + worksheet.Cells[row, 1].Value?.ToString() + ") with " + x.ToString() + " sales!";
+                        bestSaler = check;
+                    }
+                }
+                if (textBox1.Text == "") textBox1.Text = "There's no sales in this month";
+                textBox2.Text = "This month's revenue: " + revenue;
+                goodsOutDGV.DataSource = goodsOut;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            GenerateMonthlyReport(comboBox1.SelectedItem);
+        }
+    }
+}
